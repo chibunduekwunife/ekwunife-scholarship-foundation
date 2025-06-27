aafa4c110aa7584272f1433d3245d959e168eea4
chibunduekwunife <chibunduekwunife@gmail.com>
2025-06-26 22:16:45 -0400

    Restrict CORS settings: disable all origins and specify allowed origin for production

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -151,5 +151,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ekwunife-scholarship-foundation.vercel.app",
+]
+
 CORS_ALLOWS_CREDENTIALS = True
